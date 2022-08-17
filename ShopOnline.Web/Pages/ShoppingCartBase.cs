584448de52e487ee036c01068f5f9a6a3597286b
WadowickiZawadiaka <p.jachimczuk@gmail.com>
2022-08-17 14:13:12 +0200

    Added "Update qty" input field

--- ShopOnline.Web/Pages/ShoppingCartBase.cs
+++ ShopOnline.Web/Pages/ShoppingCartBase.cs
@@ -31,6 +31,39 @@ namespace ShopOnline.Web.Pages
             RemoveCartItem(id);
         }
 
+        protected async Task UpdateQtyCartItem_Click(int id, int qty)
+        {
+            try
+            {
+                if (qty > 0)
+                {
+                    var updateItemDto = new CartItemQtyUpdateDto
+                    {
+                        CartItemId = id,
+                        Qty = qty
+                    };
+
+                    var returnedUpdateItemDto = await this.ShoppingCartService.UpdateQty(updateItemDto);
+
+                }
+                else
+                {
+                    var item = this.ShoppingCartItems.FirstOrDefault(i => i.Id == id);
+
+                    if (item != null)
+                    {
+                        item.Qty = 1;
+                        item.TotalPrice = item.Price;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
         private CartItemDto GetCartItem(int id)
         {
             return ShoppingCartItems.FirstOrDefault(i => i.Id == id);
