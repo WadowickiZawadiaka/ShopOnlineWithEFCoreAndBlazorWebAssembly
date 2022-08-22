bf06e82f9b5b6480e6086187a19c55de6259e475
WadowickiZawadiaka <p.jachimczuk@gmail.com>
2022-08-22 14:11:17 +0200

    Added JS script to "Update qty" input / updating qty issues

--- ShopOnline.Web/Pages/ShoppingCartBase.cs
+++ ShopOnline.Web/Pages/ShoppingCartBase.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using ShopOnline.Models.Dtos;
 using ShopOnline.Web.Services.Contracts;
 
@@ -7,16 +8,23 @@ namespace ShopOnline.Web.Pages
     public class ShoppingCartBase : ComponentBase
     {
         [Inject]
+        public IJSRuntime Js { get; set; }
+
+        [Inject]
         public IShoppingCartService ShoppingCartService { get; set; }
         public List<CartItemDto> ShoppingCartItems { get; set; }
 
         public string ErrorMessage { get; set; }
+
+        protected string TotalPrice { get; set; }
+        protected int TotalQuantity { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
             try
             {
                 ShoppingCartItems = await ShoppingCartService.GetItems(HardCoded.UserId);
+                CalculateCartSummaryTotals();
             }
             catch (Exception ex)
             {
@@ -29,6 +37,8 @@ namespace ShopOnline.Web.Pages
             var cartItemDto = await ShoppingCartService.DeleteItem(id);
 
             RemoveCartItem(id);
+            CalculateCartSummaryTotals();
+
         }
 
         protected async Task UpdateQtyCartItem_Click(int id, int qty)
@@ -44,6 +54,11 @@ namespace ShopOnline.Web.Pages
                     };
 
                     var returnedUpdateItemDto = await this.ShoppingCartService.UpdateQty(updateItemDto);
+
+                    UpdateItemTotalPrice(returnedUpdateItemDto);
+                    CalculateCartSummaryTotals();
+
+                    await MakeUpdateQtyButtonVisible(id, false);
 
                 }
                 else
@@ -62,6 +77,43 @@ namespace ShopOnline.Web.Pages
 
                 throw;
             }
+        }
+
+        protected async Task UpdateQty_Input(int id)
+        {
+            await MakeUpdateQtyButtonVisible(id, true);
+        }
+
+        private async Task MakeUpdateQtyButtonVisible(int id, bool visible)
+        {
+            await Js.InvokeVoidAsync("MakeUpdateQtyButtonVisible", id, visible);
+        }
+
+        private void UpdateItemTotalPrice(CartItemDto cartItemDto)
+        {
+            var item = GetCartItem(cartItemDto.Id);
+
+            if (item != null)
+            {
+                item.TotalPrice = cartItemDto.Price * cartItemDto.Qty;
+            }
+
+        }
+
+        private void CalculateCartSummaryTotals()
+        {
+            SetTotalPrice();
+            SetTotalQuantity();
+        }
+
+        private void SetTotalPrice()
+        {
+            TotalPrice = this.ShoppingCartItems.Sum(p => p.TotalPrice).ToString("C");
+        }
+
+        private void SetTotalQuantity()
+        {
+            TotalQuantity = this.ShoppingCartItems.Sum(p => p.Qty);
         }
 
         private CartItemDto GetCartItem(int id)
